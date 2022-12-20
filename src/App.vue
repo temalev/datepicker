8181f30314acd2ce85cdeb360f4db624e1133e9d
Artem <myartoym@gmail.com>
2022-12-20 15:22:23 +0300

    create app datepicker

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    привет датапикер
+    <Datepicker />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Datepicker from './components/Datepicker'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Datepicker
   }
 }
 </script>
---
+++ src/components/Datepicker/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="mainContainer">
+  Это датапикер ёпт
+</div>
+</template>
+<script>
+export default {
+name: 'DataPicker'
+}
+</script>
+<style scoped>
+
+</style>
